b740d552583c701c8036225c0ba89a329e392521
Akshavanth <akshavanthm@gmail.com>
2023-08-17 17:15:49 +0530

    added some work

--- controllers/users/usersCtrl.js
+++ controllers/users/usersCtrl.js
@@ -1,4 +1,8 @@
 const User = require("../../model/User/User");
+const bcrypt = require("bcryptjs");
+
+// register user
+// end point /api/v1/users/register
 
 exports.register = async (req, res) => {
   try {
@@ -16,14 +20,54 @@ exports.register = async (req, res) => {
       password,
     });
 
+    const salt = await bcrypt.genSalt(10);
+
+    newUser.password = await bcrypt.hash(password, salt);
+
     await newUser.save();
 
     res.status(201).json({
       status: "success",
       message: "user registered sccessfully",
-      _id: newUser?.id,
-      username: newUser?.username,
-      role: newUser?.role,
+      // _id: newUser?.id,
+      // username: newUser?.username,
+      // role: newUser?.role,
+      newUser,
+    });
+  } catch (error) {
+    res.json({
+      status: "failed",
+      message: error?.message,
+    });
+  }
+};
+
+// login user
+// end point /api/v1/users/login
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    // check credential
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      throw new Error("Invalid login credentials");
+    }
+
+    //check hash password
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched) {
+      throw new Error("Invalid login credentials");
+    }
+
+    user.lastLogin = new Date();
+    await user.save();
+
+    res.status(201).json({
+      status: "success",
+      message: "user logged in sccessfully",
+      user,
     });
   } catch (error) {
     res.json({
