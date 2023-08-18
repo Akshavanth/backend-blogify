a302380385e72e3d656a3e5a36995cdf3a388c82
Akshavanth <akshavanthm@gmail.com>
2023-08-18 18:37:25 +0530

    added some chnages for error handling

--- controllers/users/usersCtrl.js
+++ controllers/users/usersCtrl.js
@@ -1,47 +1,41 @@
 const User = require("../../model/User/User");
 const bcrypt = require("bcryptjs");
 const generateToken = require("../../utils/generateToken");
+const asyncHandler = require("express-async-handler");
 
 // register user
 // end point /api/v1/users/register
 
-exports.register = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
+exports.register = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
 
-    //! check if user is already existed
-    const user = await User.findOne({ username });
-    if (user) {
-      throw new Error("User already eisted");
-    }
-
-    const newUser = new User({
-      email,
-      username,
-      password,
-    });
-
-    const salt = await bcrypt.genSalt(10);
-
-    newUser.password = await bcrypt.hash(password, salt);
-
-    await newUser.save();
-
-    res.status(201).json({
-      status: "success",
-      message: "user registered sccessfully",
-      // _id: newUser?.id,
-      // username: newUser?.username,
-      // role: newUser?.role,
-      newUser,
-    });
-  } catch (error) {
-    res.json({
-      status: "failed",
-      message: error?.message,
-    });
+  //! check if user is already existed
+  const user = await User.findOne({ username });
+  if (user) {
+    throw new Error("User already eisted");
   }
-};
+
+  const newUser = new User({
+    email,
+    username,
+    password,
+  });
+
+  const salt = await bcrypt.genSalt(10);
+
+  newUser.password = await bcrypt.hash(password, salt);
+
+  await newUser.save();
+
+  res.status(201).json({
+    status: "success",
+    message: "user registered sccessfully",
+    // _id: newUser?.id,
+    // username: newUser?.username,
+    // role: newUser?.role,
+    newUser,
+  });
+});
 
 // login user
 // end point /api/v1/users/login
@@ -85,21 +79,14 @@ exports.login = async (req, res) => {
 //get profile
 // endpoint /api/v1/users/profile/:id
 
-exports.getProfile = async (req, res) => {
+exports.getProfile = asyncHandler(async (req, res, next) => {
   const id = req.userAuth._id;
 
   const user = await User.findById(id);
-  try {
-    res.json({
-      status: "success",
-      message: "profile fetched",
-      user,
-    });
-  } catch (error) {
-    res.json({
-      status: "failed",
-      message: error?.message,
-      data: "user data",
-    });
-  }
-};
+
+  res.json({
+    status: "success",
+    message: "profile fetched",
+    user,
+  });
+});
--- server.js
+++ server.js
@@ -9,6 +9,18 @@ const app = express();
 app.use(express.json());
 app.use("/api/v1/users", usersRouter);
 
+app.use((err, req, res, next) => {
+  const status = err?.status ? err?.status : "Failed";
+  const message = err?.message;
+  const stack = err?.stack;
+
+  res.status(500).json({
+    status,
+    message,
+    stack,
+  });
+});
+
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 4000;
