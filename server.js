94f0cd8bd43198c90c7a4fbb21b090db0fd07393
Akshavanth <akshavanthm@gmail.com>
2023-08-18 19:07:04 +0530

    completed error handler

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const http = require("http");
 const express = require("express");
 const usersRouter = require("./routes/users/usersRouter");
 const connectDB = require("./config/database");
+const {
+  notFound,
+  globalErrHandler,
+} = require("./middlewares/globalErrorHandler");
 connectDB();
 
 const app = express();
@@ -9,19 +13,11 @@ const app = express();
 app.use(express.json());
 app.use("/api/v1/users", usersRouter);
 
-app.use((err, req, res, next) => {
-  const status = err?.status ? err?.status : "Failed";
-  const message = err?.message;
-  const stack = err?.stack;
-
-  res.status(500).json({
-    status,
-    message,
-    stack,
-  });
-});
-
 const server = http.createServer(app);
+
+app.use(notFound);
+
+app.use(globalErrHandler);
 
 const PORT = process.env.PORT || 4000;
 
