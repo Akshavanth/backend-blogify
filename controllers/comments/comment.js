8bc8d982d1e748f3e00c2b8dfcb78209a3d71fda
Akshavanth <akshavanthm@gmail.com>
2023-09-06 12:02:50 +0530

    completed my comments

--- controllers/comments/comment.js
+++ controllers/comments/comment.js
@@ -2,44 +2,52 @@ const asyncHandler = require("express-async-handler");
 const Comment = require("../../model/Comment/Comment");
 const Post = require("../../model/Post/Post");
 
+//@desc  Create a comment
+//@route POST /api/v1/comments/:postId
+//@access Private
+
 exports.createComment = asyncHandler(async (req, res) => {
-  const { message } = req.body;
-
+  //get the payload
+  const { message, author } = req.body;
+  //get post id from params
   const postId = req.params.postId;
-
+  //* Create comment
   const comment = await Comment.create({
     message,
     author: req.userAuth._id,
     postId,
   });
-
+  //Associate comment to a post
   await Post.findByIdAndUpdate(
     postId,
     {
-      $push: {
-        comments: comment._id,
-      },
+      $push: { comments: comment._id },
     },
-    {
-      new: true,
-    }
+    { new: true }
   );
-
+  //send the response
   res.json({
     status: "success",
-    message: "comment created successfully",
+    message: "Comment created successfully",
     comment,
   });
 });
 
+//@desc  Delete comment
+//@route DELETE /api/v1/comments/:id
+//@access Private
+
 exports.deleteComment = asyncHandler(async (req, res) => {
   await Comment.findByIdAndDelete(req.params.id);
-
   res.status(201).json({
     status: "success",
     message: "Comment successfully deleted",
   });
 });
+
+//@desc  update comment
+//@route PUT /api/v1/comments/:id
+//@access Private
 
 exports.updateComment = asyncHandler(async (req, res) => {
   const comment = await Comment.findByIdAndUpdate(
@@ -52,13 +60,6 @@ exports.updateComment = asyncHandler(async (req, res) => {
       runValidators: true,
     }
   );
-
-  console.log(req.params.id);
-
-  if (!comment) {
-    throw new Error("Comment does not exist");
-  }
-
   res.status(201).json({
     status: "success",
     message: "comment successfully updated",
--- routes/comment/commentRouter.js
+++ routes/comment/commentRouter.js
@@ -1,17 +1,20 @@
 const express = require("express");
-const isLoggin = require("../../middlewares/isLoggin");
 const {
   createComment,
-  updateComment,
   deleteComment,
+  updateComment,
 } = require("../../controllers/comments/comment");
 
+const isLoggin = require("../../middlewares/isLoggin");
 const commentRouter = express.Router();
 
+//create
 commentRouter.post("/:postId", isLoggin, createComment);
 
+//update
 commentRouter.put("/:id", isLoggin, updateComment);
 
+//delete
 commentRouter.delete("/:id", isLoggin, deleteComment);
 
 module.exports = commentRouter;
